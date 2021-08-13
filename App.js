e931acfc37e0c6ce6c255ea607bcbcc3b6375158
Jack Hales <halesyy@gmail.com>
2021-08-14 00:33:02 +1000

    goog

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { GoogleSocialButton } from "react-native-social-buttons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Jacks cool little iOS app!</Text>
+      <View style={{marginTop: 15}}>
+        <Button title="Test" color="red" />
+      </View>
+      <GoogleSocialButton />
       <StatusBar style="auto" />
     </View>
   );
